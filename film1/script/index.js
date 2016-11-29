f91edb2dd0eb303a272a5f57e8c31a135ddc3b34
1196686280 <1196686280@qq.com>
2016-11-29 18:39:10 +0800

    add title function

--- film1/script/index.js
+++ film1/script/index.js
@@ -106,4 +106,8 @@ var swiper1=new Swiper(".timenav",{
 		$(".nav_ul li").eq(a.clickedIndex).addClass("active").siblings().removeClass("active");
 		swiper1.slideTo(a.clickedIndex,300);//默认走到当前点击的li
 	}
-})
+});
+function title(){
+	console.log("这是20161128-film1-title分支上定义的方法")
+};
+
